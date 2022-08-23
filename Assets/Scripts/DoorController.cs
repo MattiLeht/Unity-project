931a6acd4f112c7563c65c73b8bf792fada37f86
VillePartanen <ville.partanen@edu.sasky.fi>
2022-08-23 17:22:06 +0300

    Ääniä oviin
    Äänet lisätty oviin. Unity Editorissa voi sääntää äänen viivettää, etäisyyttä yms. Lisäksi objektin kantaminen pelihahmolla vaatii vielä hiomista.

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     public Animator doorAnimation;
+    public AudioSource audioSource;
+    public float SoundDelay;
+    
 
     private void OnTriggerEnter(Collider other)
     {
 
         doorAnimation.SetBool("isOpening", true);
+        audioSource.PlayDelayed(SoundDelay);    
     }
 
     private void OnTriggerExit(Collider other)
     {
+        audioSource.PlayDelayed(SoundDelay);
         doorAnimation.SetBool("isOpening", false);
     }
     // Start is called before the first frame update
